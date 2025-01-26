3ea3e6f442420c8ec6e8bb96502bb8d81b66d61e
“Jasminy <jasminyduuarte_@hotmail.com>
2025-01-26 16:26:48 -0300

    update

--- MarvelDay/MarvelDay/Application/SceneDelegate.swift
+++ MarvelDay/MarvelDay/Application/SceneDelegate.swift
@@ -11,18 +11,12 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
 
-    
+
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        guard let windowScene = (scene as? UIWindowScene) else { return }
-        
-        self.window = UIWindow(frame: UIScreen.main.bounds)
-        self.window?.windowScene = windowScene
-        let navigationController = UINavigationController()
-        self.window?.rootViewController = navigationController
-        
-        let coordinator = MainCoordinator(navigationController: navigationController)
-        coordinator.start()
-        self.window?.makeKeyAndVisible()
+        // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
+        // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
+        // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
+        guard let _ = (scene as? UIWindowScene) else { return }
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
